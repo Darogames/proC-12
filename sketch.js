a1d9d9f774edf4280e94beb6dea8409eaa866fb0
Darogames <126831411+Darogames@users.noreply.github.com>
2023-03-30 17:11:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Jake1.png","Jake2.png","jake3.png","jake4.png","jake5.png");
+ }
+
+function setup(){
+  
+  createCanvas(400,400);
+ path = createSprite(200,200); 
+ path.addImage(pathImg);
+path.scale=1.2;
+
+boy = createSprite(200,200);
+boy.addAnimation(boyImg);
+boy.scale=0.08;
+  
+// crear  left Boundary (límite izquierdo)
+leftBoundary=createSprite(0,0,100,800);
+////establecer visibilidad como false (falso) para límite izquierdo
+leftBoundary.visible = false;
+//crear right Boundary (límite derecho)
+rightBoundary=createSprite(410,0,100,800);
+//establecer visibilidad como false (falso) para límite izquierdo
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background("white");
+  path.velocityY = 4;
+  
+  // boy moviéndose en el eje X con el mouse
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  // colisión de boy con los límites derecho e izquierdo invisibles 
+  
+  //código para reiniciar el fondo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
